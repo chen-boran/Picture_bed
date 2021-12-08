f24805d5fed0f43e9abcc93b5faa40c12ad63b23
chen-boran <73644649+chen-boran@users.noreply.github.com>
2021-12-08 21:31:03 +0800

    Upload by PicGo

---
+++ img/202112090531392.java
@@ -0,0 +1,15 @@
+/**
+ * @author ASUS
+ * @ 2021/12/8- 20:04
+ */
+public class Text {
+    public static void main(String args[]) {
+
+        int i = 100;
+        //String binStr = Integer.toBinaryString(i);
+        String otcStr = Integer.toOctalString(i);
+        //String hexStr = Integer.toHexString(i);
+        System.out.println(otcStr);
+
+    }
+}
